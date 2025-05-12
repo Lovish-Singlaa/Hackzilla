5b936b412704016833e5c8cadde7f6a40b082deb
Lovish Singlaa <singlalovish26@gmail.com>
2025-05-12 23:15:26 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -111,10 +111,10 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            {/* <div className="relative h-[400px] w-full rounded-lg overflow-hidden">
+            <div className="relative h-[400px] w-full rounded-lg overflow-hidden">
               <div className="absolute inset-0 bg-gradient-to-r from-red-900/80 to-black/50 z-10"></div>
-              <Image src="/hackathon-image.jpg" alt="Hackathon Environment" fill className="object-cover" />
-            </div> */}
+              <Image src="/gif.gif" alt="Hackathon Environment" fill className="object-cover" />
+            </div>
           </div>
         </div>
       </section>
@@ -199,14 +199,14 @@ export default function Home() {
             </div>
             <div className="bg-white/10 p-3 rounded-lg items-center justify-center h-32">
               <p className="text-xl font-bold text-center">Platform Partner</p>
-              <div className="w-full flex justify-center"><Image src="/sponsor2.jpg" alt="sponsor" width={200} height={30} className="object-fit"></Image></div>
+              <div className="w-full flex justify-center"><Image src="/UNSTOP_LOGO.png" alt="sponsor" width={200} height={30} className="object-fit"></Image></div>
             </div>
-            <div className="bg-white/10 p-6 rounded-lg flex items-center justify-center h-32">
+            {/* <div className="bg-white/10 p-6 rounded-lg flex items-center justify-center h-32">
               <p className="text-xl font-bold text-center">Sponsor 3</p>
             </div>
             <div className="bg-white/10 p-6 rounded-lg flex items-center justify-center h-32">
               <p className="text-xl font-bold text-center">Sponsor 4</p>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
@@ -248,13 +248,13 @@ export default function Home() {
               </h2>
             </div>
             <div className="flex space-x-4">
-              <Link href="#" className="text-gray-400 hover:text-white">
+              {/* <Link href="#" className="text-gray-400 hover:text-white">
                 <span className="sr-only">Twitter</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
-              </Link>
-              <Link href="#" className="text-gray-400 hover:text-white">
+              </Link> */}
+              <Link href="https://www.instagram.com/hack.zillaiiits/" className="text-gray-400 hover:text-white" target="blank">
                 <span className="sr-only">Instagram</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path
@@ -264,7 +264,7 @@ export default function Home() {
                   />
                 </svg>
               </Link>
-              <Link href="#" className="text-gray-400 hover:text-white">
+              <Link href="https://www.linkedin.com/posts/indian-institute-of-information-technology-iiit-sonepat_hackzilla-hackathon-unstop-activity-7326646790152146944-yD90/?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAEYv_n4BNByNBpZgc58RYSB47OrTv0i_tuQ" className="text-gray-400 hover:text-white" target="blank">
                 <span className="sr-only">LinkedIn</span>
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
